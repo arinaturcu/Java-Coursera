11ce4de30179f2f4086112827eea949a57c234b8
arinaturcu <arinaturcu2@gmail>
2020-09-18 20:13:47 +0300

    initial commit

--- test/TestSimple.java
+++
@@ -1,6 +0,0 @@
-public class TestSimple {
-    public static void main(String argv[]) {  	
-        Simple item = new Simple(3, "blue");     	
-        System.out.println(item);             
-   } 
-}
